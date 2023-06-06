b64d6fb9a79e17a4a99c884bfe2a94c2018c0574
Yevheniy Petrenko <yevheniipetrenko92@gmail.com>
2023-06-06 10:17:46 +0200

    added poetry and Dockerfile

--- __main__.py
+++ __main__.py
@@ -3,13 +3,31 @@ from abc import ABC, abstractmethod
 
 class AbstractBot(ABC):
     @abstractmethod
-    def handle(self, command):
+    def handle(self, action):
         pass
+
+
+class Book:
+    def load(self, file_name):
+        print(f"Loading contacts from {file_name}")
+
+    def save(self, file_name):
+        print(f"Saving contacts to {file_name}")
 
 
 class Bot(AbstractBot):
-    def handle(self, command):
-        pass
+    def __init__(self):
+        self.book = Book()
+
+    def handle(self, action):
+        if action == 'add':
+            print("Adding a contact...")
+        elif action == 'remove':
+            print("Removing a contact...")
+        elif action == 'edit':
+            print("Editing a contact...")
+        else:
+            print("Invalid command")
 
 
 if __name__ == "__main__":
@@ -18,7 +36,7 @@ if __name__ == "__main__":
     bot.book.load("auto_save")
     commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
     while True:
-        action = input('Type help for list of commands or enter your command\n').strip().lower()
+        action = input('Type help for a list of commands or enter your command:\n').strip().lower()
         if action == 'help':
             format_str = str('{:%s%d}' % ('^', 20))
             for command in commands:
